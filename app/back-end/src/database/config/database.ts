b69fba385a84cf10893142dea2c5bae31796d846
kaellAlecs <kaellaleksander@gmail.com>
2022-11-15 14:28:52 -0300

    Iniciando projeto

---
+++ app/back-end/src/database/config/database.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+import { Options } from 'sequelize';
+
+const config: Options = {
+  username: process.env.DB_USER || 'root',
+  password: process.env.DB_PASS || 'password',
+  database: 'NG_CASH',
+  host: process.env.DB_HOST || 'db',
+  port: Number(process.env.DB_PORT) || 3002,
+  dialect: 'postgres',
+  dialectOptions: {
+    timezone: 'Z',
+  },
+  logging: false,
+}
+
+module.exports = config;
---
+++ app/back-end/src/database/models/user.ts
@@ -0,0 +1,20 @@
+// 'use strict';
+// const {
+//   Model
+// } = require('sequelize');
+// module.exports = (sequelize, DataTypes) => {
+//   class User extends Model {
+    
+
+//     }
+//   }
+//   User.init({
+//     firstName: DataTypes.STRING,
+//     lastName: DataTypes.STRING,
+//     email: DataTypes.STRING
+//   }, {
+//     sequelize,
+//     modelName: 'User',
+//   });
+//   return User;
+// };
